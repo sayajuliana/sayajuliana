4c92e95dc07f962b8d1ee6cd5a72a2f12d1aaab2
Juliana Leal Páez <julianyta0325@hotmail.com>
2021-10-06 22:30:06 -0500

    actualizacion ajedrez

--- ajedrezReto.js
+++ ajedrezReto.js
@@ -1,47 +1,102 @@
-const tablerito = document.querySelector('#tablero');
-const cantidadColumnas = document.getElementById('txt-columnas');
-const cantidadFilas = document.getElementById('txt-filas');
-const btnCrear = document.getElementById('btn-crear');
+const table= document.querySelector('#table1');
+const numberColumns = document.getElementById('txt-columns');
+const numberRow = document.getElementById('txt-row');
+const btnCreate = document.getElementById('btn-create');
 
 
-btnCrear.addEventListener('click',dibujarTablero);
+/*btnCrear.addEventListener('click',dibujarTablero);*/
 
-
-function dibujarTablero() {
-    const columnas = Number(cantidadColumnas.value);
-    if((columnas%2 != 0) || (columnas < 8)){
+//Dibujar el tablero
+function paintTable() {
+    const column = Number(numberColumns.value);
+    if((column%2 != 0) || (column < 8)){
         alert("Dato Invalido!!!\nPor favor ingrese un numero par y que sea mayor o igual a 8");
         return;
         }
-    const filas = Number(cantidadFilas.value);
-    if((filas%2 != 0) || (filas < 4)){
+
+    const row = Number(numberRow.value);
+    if((row%2 != 0) || (row< 4)){
         alert("Dato Invalido!!!\nPor favor ingrese un numero par y que sea mayor o igual a 4");
         return;
         }
 
-    console.log(columnas);
-    console.log(filas);
+    let arrDrawers2 = initialBoard(row, column);
 
-    let arrCajones = new Array (filas);
-    for (let index = 0; index < filas; index++) {
-        arrCajones[index]=new Array (columnas);        
+    console.log(column);
+    console.log(row);
+    console.log(arrDrawers2);
+
+    let arrDrawers = new Array (row);
+    for (let index = 0; index < row; index++) {
+        arrDrawers[index]=new Array (column);        
     }
-    
-    let tabla = '<tabla>'
-    for (let i = 0; i < filas; i++) {
-        const row = document.createElement('div'); 
-        row.classList.add('fil');
-        tablerito.appendChild (row);
 
-        for (let j = 0; j < columnas; j++){
+    //Pintar el tablero
+    table.style.width = 50 *column + 'px'; 
+    table.style.height = 50 *row + 'px';
+
+
+    for (let i = 0; i < row; i++) {
+        const row = document.createElement('div'); 
+        row.classList.add('row');
+        table.appendChild (row);
+
+        for (let j = 0; j < column; j++){
             const col = document.createElement('div');
-            col.classList.add('cuadrito');
-            tablerito.appendChild(col);
-            arrCajones[i][j]=col.innerHTML;
+            col.classList.add('cell');
+            row.appendChild(col);
+            arrDrawers[i][j]=col.innerHTML;
+            if ((i+j)%2 != 0){
+                col.style.backgroundColor = '#01444d';
+            } else {
+                col.style.background = '#d7f6fa'
+            }
+            
         }
         
     }
-    console.log(arrCajones);
-
+    console.log(row);
+    console.log(arrDrawers);
 }
+
+function initialBoard (f,c){
+
+    
+    let t = new Array(f);
+    console.log (f);
+     
+    for (let i = 0; i < f; i++) {
+       t[i] = new Array(c).fill(" ");
+    }
+    
+    for(i=0;i<8;i++){
+        t[1][(c/2)-4+i]= "B-P";
+        t[f-2][(c/2)-4+i]= "W-P";
+    }
+
+    for(i=0;i<2;i++){
+        t[0][(c/2)-4+7*i]= "B-T";
+        t[f-1][(c/2)-4+7*i]= "W-T";
+    }
+
+    for(i=0;i<2;i++){
+        t[0][(c/2)-3+5*i]= "B-N";
+        t[f-1][(c/2)-3+5*i]= "W-N";
+    }
+
+    for(i=0;i<2;i++){
+        t[0][(c/2)-2+3*i]= "B-B";
+        t[f-1][(c/2)-2+3*i]= "W-B";
+    }
+
+    t[0][(c/2)-1]= "B-Q";
+    t[f-1][(c/2)-1]= "W-Q";
+    t[0][(c/2)]= "B-K";
+    t[f-1][(c/2)]= "W-K";
+
+    return (t);
+     
+}
+
+
 
